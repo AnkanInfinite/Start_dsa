f30fefa35efad37298c79a31fb75b9985e8261cc
AnkanInfinte <ankankundu907@gmail.com>
2026-03-01 17:53:04 +0000

    Relaion between number of rows and number of stars

--- basic/pattern/q8.cpp
+++ basic/pattern/q8.cpp
@@ -1,1 +1,20 @@
 #include<iostream>
+using namespace std;
+void print8(int n){
+    int nstars=2*n -1;
+    for(int rows=0;rows<n;rows++){
+        for(int space=0;space<rows;space++){
+            cout << " ";
+        }
+        for(int stars=0;stars<nstars;stars++){
+            cout << "*";
+        }
+        nstars-=2;
+        cout << endl;
+    }
+}
+
+int main(){
+    print8(5);
+    return 0;
+}
