ca4fcd0eb40db3b4afca46becdedc11d6baa0cf0
AnkanInfinte <ankankundu907@gmail.com>
2026-06-26 15:32:38 +0000

    Produced solution in Single pass and edge case where Number of positives and negatives is not equal

--- array/medium/rearrange.cpp
+++ array/medium/rearrange.cpp
@@ -12,7 +12,7 @@ that the given conditions are met:
 Approach 1 :
 Create two arrays to store positive and negative elements preseving their relative order
 Overide The oiginal array as required
-Time Complexity : O(N)
+Time Complexity : O(N)  or O(2N) to be specific
 Space Complexity : O(N)
 */
 
@@ -47,4 +47,67 @@ std::vector<int> rearrangeArray_1(std::vector<int> &nums){
     and fo odd positions 2*i + 1
     */
     return nums;
+}
+
+/*
+Approach 2 : Single pass approach
+Since we know that all positive elements would be stored in even positions of result array
+and negative in odd positions
+We can create of length same as given array and put elements in correct positions as required
+Time Complexity : O(N)
+Space Complexity : O(N)
+*/
+
+std::vector<int> rearrangeArray_2(const std::vector<int> &nums){
+    int n=nums.size();
+    std::vector<int> ans(n,0);
+    int posIndex=0;
+    int negIndex=1;
+    for(auto el : nums){
+        if(el < 0){
+            ans[negIndex]=el;
+            negIndex+=2;
+        }
+        else{
+            ans[posIndex]=el;
+            posIndex+=2;
+        }
+    }
+    return ans;
+}
+
+/*
+In case Number of Positive elements is not equal to Number of negative elements
+And we are required keep any left over positive(negative) elements at end of result array 
+preserving its relative order
+Time Complexity : around O(2N)
+Space Complexity : O(2N)
+*/
+
+std::vector<int> rearrangeArray_3(const std::vector<int> &nums){
+    int n=nums.size();
+    std::vector<int> posElements;
+    std::vector<int> negElements;
+    std::vector<int> ans;
+    for(auto el : nums){
+        if(el<0){
+            negElements.push_back(el);
+        }
+        else{
+            posElements.push_back(el);
+        }
+    }
+    int equalNumber=std::min(negElements.size(),posElements.size());
+    for(int i=0;i<equalNumber;i++){
+        ans.push_back(posElements[i]);
+        ans.push_back(negElements[i]);
+    }
+    //Now left over elements
+    for(int i=equalNumber;i<posElements.size();i++){
+        ans.push_back(posElements[i]);
+    }
+    for(int i=equalNumber;i<negElements.size();i++){
+        ans.push_back(negElements[i]);
+    }
+    return ans;
 }
